d4c872632baab6b17440b9f1c517bdad9752f098
Raúl Camacho <raulj.camacho@gmail.com>
2019-12-29 15:03:56 -0500

    Se agrego comentarios a archivo main

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
-  key_name = "camarj"
-  vpc_security_group_ids = ["sg-0fe75f088a2d00c5a"]
+  key_name = "camarj"       #Nombre de tu llave de aws
+  vpc_security_group_ids = ["sg-0fe75f088a2d00c5a"]     #Id de el security group
 
   tags = {name = "test-instance"}
 }
